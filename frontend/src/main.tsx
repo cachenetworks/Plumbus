594a48727d469206f86a67cce03c275a7647ebe3
CacheNetworks <43885817+cachenetworks@users.noreply.github.com>
2026-08-31 20:23:57 +1000

    Route catalog pages through optimized streaming UI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import { CatalogApp } from './CatalogApp'
 import { PlaybackToolbar } from './PlaybackToolbar'
 import { SetupWizard } from './SetupWizard'
 import { WatchPage } from './WatchPage'
@@ -10,9 +11,11 @@ import './setup.css'
 import './crt.css'
 import './media.css'
 
-const isSetup = window.location.pathname === '/setup'
-const isWatch = /^\/watch\/\d+/.test(window.location.pathname)
-const Root = isSetup ? SetupWizard : isWatch ? WatchPage : App
+const path = window.location.pathname
+const isSetup = path === '/setup'
+const isWatch = /^\/watch\/\d+/.test(path)
+const isCatalog = path === '/browse' || path === '/search' || path === '/collections' || /^\/(media|movie)\/\d+$/.test(path)
+const Root = isSetup ? SetupWizard : isWatch ? WatchPage : isCatalog ? CatalogApp : App
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
