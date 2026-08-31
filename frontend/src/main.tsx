c353a5e3abef24dff3879b95257bb65746fd5525
CacheNetworks <43885817+cachenetworks@users.noreply.github.com>
2026-08-31 20:25:56 +1000

    Switch optimized catalog on client-side navigation

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, useLocation } from 'react-router-dom'
 import App from './App'
 import { CatalogApp } from './CatalogApp'
 import { PlaybackToolbar } from './PlaybackToolbar'
@@ -11,17 +11,19 @@ import './setup.css'
 import './crt.css'
 import './media.css'
 
-const path = window.location.pathname
-const isSetup = path === '/setup'
-const isWatch = /^\/watch\/\d+/.test(path)
-const isCatalog = path === '/browse' || path === '/search' || path === '/collections' || /^\/(media|movie)\/\d+$/.test(path)
-const Root = isSetup ? SetupWizard : isWatch ? WatchPage : isCatalog ? CatalogApp : App
+function RoutedRoot(){
+  const {pathname}=useLocation()
+  const isSetup=pathname==='/setup'
+  const isWatch=/^\/watch\/\d+/.test(pathname)
+  const isCatalog=pathname==='/browse'||pathname==='/search'||pathname==='/collections'||/^\/(media|movie)\/\d+$/.test(pathname)
+  return <>
+    {isSetup?<SetupWizard/>:isWatch?<WatchPage/>:isCatalog?<CatalogApp/>:<App/>}
+    {!isSetup&&!isWatch&&<PlaybackToolbar/>}
+  </>
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Root />
-      {!isSetup && !isWatch && <PlaybackToolbar />}
-    </BrowserRouter>
+    <BrowserRouter><RoutedRoot/></BrowserRouter>
   </React.StrictMode>,
 )
