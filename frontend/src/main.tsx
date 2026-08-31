69766d3e347ad33cd58a14c96a1a9bd86c03ba8e
CacheNetworks <43885817+cachenetworks@users.noreply.github.com>
2026-08-31 14:57:54 +1000

    Route first-run setup through React wizard

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import { SetupWizard } from './SetupWizard'
 import './styles.css'
+
+const Root = window.location.pathname === '/setup' ? SetupWizard : App
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Root />
     </BrowserRouter>
   </React.StrictMode>,
 )
