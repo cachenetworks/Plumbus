55298d61b78616f7a649eaf32300471f75d094d7
CacheNetworks <43885817+cachenetworks@users.noreply.github.com>
2026-08-31 20:36:14 +1000

    Fix catalog media route params

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, useLocation } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import App from './App'
 import { CatalogApp } from './CatalogApp'
 import { PlaybackToolbar } from './PlaybackToolbar'
@@ -11,13 +11,23 @@ import './setup.css'
 import './crt.css'
 import './media.css'
 
+function CatalogRoutes(){
+  return <Routes>
+    <Route path="/browse" element={<CatalogApp/>}/>
+    <Route path="/search" element={<CatalogApp/>}/>
+    <Route path="/collections" element={<CatalogApp/>}/>
+    <Route path="/media/:id" element={<CatalogApp/>}/>
+    <Route path="/movie/:id" element={<CatalogApp/>}/>
+  </Routes>
+}
+
 function RoutedRoot(){
   const {pathname}=useLocation()
   const isSetup=pathname==='/setup'
   const isWatch=/^\/watch\/\d+/.test(pathname)
   const isCatalog=pathname==='/browse'||pathname==='/search'||pathname==='/collections'||/^\/(media|movie)\/\d+$/.test(pathname)
   return <>
-    {isSetup?<SetupWizard/>:isWatch?<WatchPage/>:isCatalog?<CatalogApp/>:<App/>}
+    {isSetup?<SetupWizard/>:isWatch?<WatchPage/>:isCatalog?<CatalogRoutes/>:<App/>}
     {!isSetup&&!isWatch&&<PlaybackToolbar/>}
   </>
 }
