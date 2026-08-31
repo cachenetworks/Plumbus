2202aa45beecb7d623f02bedf250a734a247000b
CacheNetworks <43885817+cachenetworks@users.noreply.github.com>
2026-08-31 18:40:58 +1000

    Wire browser watch page and playback toolbar

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,18 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import { PlaybackToolbar } from './PlaybackToolbar'
 import { SetupWizard } from './SetupWizard'
+import { WatchPage } from './WatchPage'
 import './styles.css'
 import './setup.css'
 import './crt.css'
 import './media.css'
 
-const Root = window.location.pathname === '/setup' ? SetupWizard : App
+const isSetup = window.location.pathname === '/setup'
+const isWatch = /^\/watch\/\d+/.test(window.location.pathname)
+const Root = isSetup ? SetupWizard : isWatch ? WatchPage : App
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Root />
+      {!isSetup && !isWatch && <PlaybackToolbar />}
     </BrowserRouter>
   </React.StrictMode>,
 )
