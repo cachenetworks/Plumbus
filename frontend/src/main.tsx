1bd14d7e8be0a8fdcc7846b99800f17fb98d0174
CacheNetworks <43885817+cachenetworks@users.noreply.github.com>
2026-09-02 11:18:58 +1000

    Fix catalog watched and categories routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,11 +10,25 @@ import './setup.css'
 import './crt.css'
 import './media.css'
 
+const catalogRootPaths = new Set([
+  '/browse',
+  '/search',
+  '/categories',
+  '/collections',
+  '/watched',
+])
+
+function isCatalogPath(pathname:string){
+  return catalogRootPaths.has(pathname)||/^\/(media|movie)\/\d+$/.test(pathname)
+}
+
 function CatalogRoutes(){
   return <Routes>
     <Route path="/browse" element={<CatalogApp/>}/>
     <Route path="/search" element={<CatalogApp/>}/>
+    <Route path="/categories" element={<CatalogApp/>}/>
     <Route path="/collections" element={<CatalogApp/>}/>
+    <Route path="/watched" element={<CatalogApp/>}/>
     <Route path="/media/:id" element={<CatalogApp/>}/>
     <Route path="/movie/:id" element={<CatalogApp/>}/>
   </Routes>
@@ -24,7 +38,7 @@ function RoutedRoot(){
   const {pathname}=useLocation()
   const isSetup=pathname==='/setup'
   const isWatch=/^\/watch\/\d+/.test(pathname)
-  const isCatalog=pathname==='/browse'||pathname==='/search'||pathname==='/collections'||/^\/(media|movie)\/\d+$/.test(pathname)
+  const isCatalog=isCatalogPath(pathname)
   return isSetup?<SetupWizard/>:isWatch?<WatchPage/>:isCatalog?<CatalogRoutes/>:<App/>
 }
 
